971881dd6a219809cd95ccc9e90b1df4d975c5d4
Rogix <12768403+rogix@users.noreply.github.com>
2022-01-20 22:03:47 -0300

    feat: add BlogPost page

---
+++ web/src/pages/BlogPostPage/BlogPostPage.js
@@ -0,0 +1,21 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const BlogPostPage = () => {
+  return (
+    <>
+      <MetaTags title="BlogPost" description="BlogPost page" />
+
+      <h1>BlogPostPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/BlogPostPage/BlogPostPage.js</code>
+      </p>
+      <p>
+        My default route is named <code>blogPost</code>, link to me with `
+        <Link to={routes.blogPost()}>BlogPost</Link>`
+      </p>
+    </>
+  )
+}
+
+export default BlogPostPage
