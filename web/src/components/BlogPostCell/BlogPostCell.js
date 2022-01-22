fbd57363b211f224383155ea42ebadd143bcb5c6
Rogix <12768403+rogix@users.noreply.github.com>
2022-01-22 18:36:07 -0300

    feat: using the param

---
+++ web/src/components/BlogPostCell/BlogPostCell.js
@@ -0,0 +1,22 @@
+export const QUERY = gql`
+  query BlogPostQuery($id: Int!) {
+    post(id: $id) {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ post }) => {
+  return <div>{JSON.stringify(post)}</div>
+}
--- web/src/pages/BlogPostPage/BlogPostPage.js
+++ web/src/pages/BlogPostPage/BlogPostPage.js
@@ -1,21 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import BlogPostCell from 'src/components/BlogPostCell'
 
-const BlogPostPage = () => {
-  return (
-    <>
-      <MetaTags title="BlogPost" description="BlogPost page" />
-
-      <h1>BlogPostPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/BlogPostPage/BlogPostPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>blogPost</code>, link to me with `
-        <Link to={routes.blogPost()}>BlogPost</Link>`
-      </p>
-    </>
-  )
+const BlogPostPage = ({ id }) => {
+  return <BlogPostCell id={id} />
 }
 
 export default BlogPostPage
